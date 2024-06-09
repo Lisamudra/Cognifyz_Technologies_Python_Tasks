781882f5be3ca973ec59c85cf495d6e2cc8dc2b3
LISAMUDRA SWAIN <161136113+Lisamudra@users.noreply.github.com>
2024-06-09 11:29:28 +0530

    Add files via upload

---
+++ reverse_string.py
@@ -0,0 +1,9 @@
+def reverse_string(word):
+    return word[::-1]
+
+string = input("Enter a String: ")
+
+print("The Original String is : ", string)
+print("The Reversed String is : ", reverse_string(string))
+
+print("==<Thank You For Using>==")
